61d51bdfa7f015b07e06af527d1b40bc1b693980
Uzain Ali <muzainalisiddiqui@gmail.com>
2025-05-06 17:31:06 +0500

    user authentication apis

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserAuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,11 +23,16 @@ Route::get("/text", function () {
     return ["name"=>"uzain", "email" => "uzain@gmail.com", "age" => 23];
 });
 
-Route::get("/student", [StudentController::class, "list"]);
-Route::post("/addStudent", [StudentController::class, "addStudent"]);
-Route::put("/updateStudent", [StudentController::class, "updateStudent"]);
-Route::delete("/deleteStudent/{id}", [StudentController::class, "deleteStudent"]);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get("/student", [StudentController::class, "list"]);
+    Route::post("/addStudent", [StudentController::class, "addStudent"]);
+    Route::put("/updateStudent", [StudentController::class, "updateStudent"]);
+    Route::delete("/deleteStudent/{id}", [StudentController::class, "deleteStudent"]);
+    Route::resource('members', MemberController::class);
+});
+
 Route::get("/searchStudent/{name}", [StudentController::class, "searchStudent"]);
 
-
-Route::resource('members', MemberController::class);
+Route::post("/signup", [UserAuthController::class, "signup"]);
+Route::post("/login", [UserAuthController::class, "login"])->name('login');
